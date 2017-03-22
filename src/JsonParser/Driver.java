238c1719810699cac28b472c1e8670060ad7f4f9
ALAIN\Richard <rich.grandmaison@gmail.com>
2017-03-21 21:11:56 -0400

    more stats

--- src/JsonParser/Driver.java
+++ src/JsonParser/Driver.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import com.google.gson.*;
 
@@ -21,17 +24,30 @@ public class Driver {
 		BufferedReader br = new BufferedReader(new FileReader(data));
 		String line;
 		HashMap<String, Integer> reviewsPerUser = new HashMap<String, Integer>();
+		HashMap<String, Integer> reviewsPerItem = new HashMap<String, Integer>();
+		HashMap<Float, Integer> ratingDistribution = new HashMap<Float, Integer>();
 		
 		float totalRating = 0;
 		int numberOfReview = 0;
 		int maxNumberOfReviewsForAUser = 0;
+		int maxNumberOfReviewsForAnItem = 0;
 		String maxNumberOfReviewsUser = null;
+		String maxNumberOfReviewsItem = null;
 		
 		while((line = br.readLine()) != null){
 			review = gson.fromJson(line, Review.class);
 			review.getRating();
 			numberOfReview++;
-			totalRating += review.rating;	
+			totalRating += review.rating;
+			if(totalRating < 1){ // just to print sample for analysis
+				System.out.println(line + "\n");
+			}
+			
+			if(!ratingDistribution.containsKey(review.rating)){
+				ratingDistribution.put(review.rating, 1);
+			} else{
+				ratingDistribution.put(review.rating, ratingDistribution.get(review.rating) + 1);
+			}
 			
 			if(!reviewsPerUser.containsKey(review.reviewerID)){
 				reviewsPerUser.put(review.reviewerID, 1);
@@ -41,14 +57,37 @@ public class Driver {
 			if(reviewsPerUser.get(review.reviewerID) > maxNumberOfReviewsForAUser){
 				maxNumberOfReviewsForAUser = reviewsPerUser.get(review.reviewerID);
 				maxNumberOfReviewsUser = review.reviewerID;
-			}			
+			}	
+			
+			if(!reviewsPerItem.containsKey(review.asin)){
+				reviewsPerItem.put(review.asin, 1);
+			} else{
+				reviewsPerItem.put(review.asin, reviewsPerItem.get(review.asin) + 1);
+			}
+			if(reviewsPerItem.get(review.asin) > maxNumberOfReviewsForAnItem){
+				maxNumberOfReviewsForAnItem = reviewsPerItem.get(review.asin);
+				maxNumberOfReviewsItem = review.asin;
+			}
 		}
 		br.close();
 		
-		System.out.println("Number of reviews: " + numberOfReview);
+		SortedSet<Float> keys = new TreeSet<Float>(ratingDistribution.keySet());
+		for (Float key : keys) { 
+		   Integer value = ratingDistribution.get(key);
+		   double percentage = ((value * 100)/numberOfReview);
+		   System.out.println(value + " ratings for score of " + key + ". (" + percentage + "%)");
+		}
+		
+		System.out.println("\nNumber of reviews: " + numberOfReview);
+		System.out.println("Number of reviewers: " + reviewsPerUser.size());
+		System.out.println("Number of items: " + reviewsPerItem.size() + "\n");
 		System.out.println("average rating: " + totalRating/numberOfReview);
-		System.out.println(maxNumberOfReviewsUser + " is leading with " + maxNumberOfReviewsForAUser + " reviews.");
-		System.out.println(maxNumberOfReviewsUser + " is leading with " + maxNumberOfReviewsForAUser + " reviews.");
+		System.out.println("average ratings per user: " + (float)numberOfReview/reviewsPerUser.size());
+		System.out.println("average ratings per item: " + (float)numberOfReview/reviewsPerItem.size());
+
+		System.out.println(maxNumberOfReviewsUser + " is the user leading with " + maxNumberOfReviewsForAUser + " reviews.");
+		System.out.println(maxNumberOfReviewsItem + " is the item leading with " + maxNumberOfReviewsForAnItem + " reviews.");
 		}		
 	}
+
 
